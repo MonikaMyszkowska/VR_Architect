45766ae63965ae3da07117208c209a8f1901cac1
Monika Myszkowska <monikamyszkowska01@gmail.com>
2022-07-20 16:46:50 +0200

    inicialize project

---
+++ Assets/FurnitureButton.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FurnitureButton : GazeableButton {
+
+    public Object prefab;
+
+    public override void OnPress(RaycastHit hitInfo)
+    {
+        base.OnPress(hitInfo);
+
+        Player.instance.activeMode = InputMode.FURNITURE;
+
+        Player.instance.activeFurniturePrefab = prefab;
+        
+    }
+}
---
+++ Assets/GrunwaldControler.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrunwaldControler : MonoBehaviour
+{
+    public AudioSource audioSource;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void AudioPlay(AudioClip otherClip)
+    {
+        if (!audioSource.isPlaying)
+        {
+            audioSource.clip = otherClip;
+            audioSource.Play();
+        }
+    }
+
+}
